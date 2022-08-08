3081127f865b65ca16df9726ad3fa71983f89999
ernes <santimayer.git@gmail.com>
2022-08-08 15:31:20 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import pygame
+pygame.init()
+dis=pygame.display.set_mode((400, 600))
+pygame.display.update()
+pygame.display.set_caption("Snake Game")
+game_over=False
+while not game_over:
+    for event in pygame.event.get():
+        print(event)
+pygame.quit()
+quit()
